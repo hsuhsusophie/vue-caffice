2e9ac6c7caeac92f514fc340c6d7ab11e2b9748a
“your <hsuhsusophie@gmail.com>
2024-02-19 19:43:01 +0800

    adjust base

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "vue-caffice",
+  base: '/vue-caffice/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
